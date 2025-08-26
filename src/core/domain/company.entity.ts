629a24ae0df4f211bbf4e76093399a63cef74fbf
nairalucila <nairacoppari@gmail.com>
2025-08-26 17:20:14 -0300

    feature: functionality added some mocks implemented

--- src/core/domain/company.entity.ts
+++ src/core/domain/company.entity.ts
@@ -1,18 +1,16 @@
-// export interface Company {
-//   id: string;
-//   name: string;
-//   type: string;
-//   adhesionDate: string;
-//   transferDates: string[];
-// }
+export enum CompanyType {
+  Pyme = 'Pyme',
+  Corporativa = 'Corporativa',
+}
 
-//TODO: aclarar que se uso el tipo string para las fechas a modo piloto ya que los datos de obtienen de un json.
 export class Company {
   constructor(
     public readonly id: string,
-    public readonly nombre: string,
-    public readonly tipo: 'Pyme' | 'Corporativa',
-    public readonly fechaAdhesion: string,
-    public readonly fechasTransferencias: string[] = [],
+    public readonly name: string,
+    public readonly type: CompanyType,
+    public readonly adhesionDate: string,
+    public readonly transferDates: string[],
   ) {}
 }
+
+//TODO: aclarar que se uso el tipo string para las fechas a modo piloto ya que los datos de obtienen de un json.
--- src/infraestructure/persistance/company.repository.ts
+++ src/infraestructure/persistance/company.repository.ts
@@ -1,21 +1,60 @@
-/* eslint-disable @typescript-eslint/require-await */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Company } from 'src/core/domain/company.entity';
 import { ICompanyRepository } from 'src/core/application/ports/out/company.repository.interface';
+import { promises as fs } from 'fs';
+import path from 'path';
+import { CompanyType } from 'src/core/domain/company.entity';
 
+//TODO: Recordar eliminar los disables de Eslint.
 export class CompanyRepository implements ICompanyRepository {
-  //TODO: Recordar eliminar los disables de Eslint.
+  private readonly filePath: string = path.join(
+    __dirname,
+    'utils',
+    'json-company-data.json',
+  );
 
   async getCompaniesByLastMonthTransfers(): Promise<Company[]> {
-    // Lógica para leer el JSON y convertirlo en objetos Empresa
-    return [];
+    //TODO: Añadir metetodo para leer los files.
+    // - Falta agregar el servicio para que filtre por mes.
+    const empresa = new Company(
+      '5',
+      'Mate.io.',
+      CompanyType.Pyme,
+      '2024-07-15T10:00:00Z',
+      ['2025-03-20T11:30:00Z'],
+    );
+
+    return Promise.resolve([empresa]);
   }
 
   async getCompaniesByAdhesionDate(): Promise<Company[]> {
-    return [];
+    //TODO: COMPLETAR ESTO
+    const empresa = new Company(
+      '5',
+      'Mate.io.',
+      CompanyType.Pyme,
+      '2024-07-15T10:00:00Z',
+      ['2025-03-20T11:30:00Z'],
+    );
+
+    return Promise.resolve([empresa]);
   }
 
   async addCompany(company: Company): Promise<void> {
+    //TODO:esto es simulado
+    const empresa = new Company(
+      '7',
+      'Simulador.io.',
+      CompanyType.Corporativa,
+      '2024-07-15T10:00:00Z',
+      ['2025-03-20T11:30:00Z'],
+    );
+
+    const emp = await fs.writeFile(
+      this.filePath,
+      JSON.stringify(empresa, null, 2),
+    );
+
     return;
   }
 }
